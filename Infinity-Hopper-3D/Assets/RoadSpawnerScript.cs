cbc61c7d234e8870631ce34c614a7823834aea56
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-10-02 13:03:49 -0500

    created roadSpawnerScript

---
+++ Infinity-Hopper-3D/Assets/RoadSpawnerScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject road;
+    LogicScript logicScript;
+    // access countUntillNewRoad from logic
+    int countUntillNewRoad;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // access logic
+        logicScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roadSpawner();
+    }
+
+    private void roadSpawner()
+    {
+        countUntillNewRoad = logicScript.countUntillNewRoad;
+
+        // if countUntillNewRoad is at 4
+        if (countUntillNewRoad == 4)
+        {
+            // spawn new road
+            createNewRoad();
+            // reset countUntillNewRoad to 1
+            logicScript.countUntillNewRoad = 0;
+        }
+    }
+
+    private void createNewRoad()
+    {
+        Instantiate(road, transform.position, transform.rotation);
+    }
+
+    //THOUGHTS ON WHAT THIS SCRIPT IS SUPPOSED TO DO
+    //Create new roads periodically
+
+}
