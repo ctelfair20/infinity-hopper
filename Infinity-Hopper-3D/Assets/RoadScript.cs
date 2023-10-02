2c071f230aeb23fe0a4317003d0ae0f107ecf235
Cynthia-ann Telfair <ctelfair20@gmail.com>
2023-10-02 13:04:06 -0500

    created road script

---
+++ Infinity-Hopper-3D/Assets/RoadScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadScript : MonoBehaviour
+{
+    Vector3 roadPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roadMovement();
+    }
+
+    private void roadMovement()
+    {
+        getRoadPosition();
+
+        //if()
+
+    }
+
+    private void getRoadPosition()
+    {
+        roadPosition = transform.position;
+    }
+
+    //THOUGHTS ON WHAT THIS SCRIPT IS SUPPOSED TO DO
+    //Tell the road to move toward the frog on touch and keyboard press lane by lane
+}
