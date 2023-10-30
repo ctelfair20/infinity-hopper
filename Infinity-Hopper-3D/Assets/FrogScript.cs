0ea719250d3e10d1e4d388ccd5f46973e2675621
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-10-30 13:43:18 -0500

    Update FrogScript.cs
    clean up

--- Infinity-Hopper-3D/Assets/FrogScript.cs
+++ Infinity-Hopper-3D/Assets/FrogScript.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class FrogScript : MonoBehaviour
 {
-    //LogicScript logicScript;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //logicScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-    }
-
     // Update is called once per frame
     void Update()
     {
