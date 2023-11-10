352ea535d716b1e44695afd4a2b51a941bf569cd
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-10 16:13:02 -0600

    created basis ui controller comp

---
+++ Infinity-Hopper-3D/Assets/UIControllerScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControllerScript : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public GameObject gameOverScreen;
+    public GameObject activeGameUIScreen;
+    public GameObject homeScreen;
+    public GameObject highScoreListScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Toggle Functions
+    public void togglePauseScreen()
+    {
+        
+    }
+
+    public void toggleGameOverScreen()
+    {
+
+    }
+
+    public void toggleHomeScreen()
+    {
+
+    }
+
+    public void toggleHighScoreListScreen()
+    {
+
+    }
+}
