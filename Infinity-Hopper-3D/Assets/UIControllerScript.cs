ac9c7b9e55b9b7771bfba2cbba003a65d7d40327
Cynthia-ann Telfair <73765398+ctelfair20@users.noreply.github.com>
2023-11-13 19:21:57 -0600

    condensed toggle functions for the ui controller

--- Infinity-Hopper-3D/Assets/UIControllerScript.cs
+++ Infinity-Hopper-3D/Assets/UIControllerScript.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Device;
 
 public class UIControllerScript : MonoBehaviour
 {
     public GameObject pauseScreen;
-    public GameObject gameOverScreen;
-    public GameObject activeGameUIScreen;
     public GameObject homeScreen;
+    public GameObject gameOverScreen;
     public GameObject highScoreListScreen;
+    public GameObject activeGameUIScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // turn on homescreen
+        toggleScreen(homeScreen);
     }
 
     // Update is called once per frame
@@ -23,23 +25,28 @@ public class UIControllerScript : MonoBehaviour
     }
 
     //Toggle Functions
-    public void togglePauseScreen()
+    public void turnOffHomescreen()
     {
-        
+        // deactivate home
+        toggleScreen(homeScreen);
+        // activate the active game ui
+        toggleScreen(activeGameUIScreen);
     }
 
-    public void toggleGameOverScreen()
+    public void onFrogDeath()
     {
-
+        // activate gameover screen
+        toggleScreen(gameOverScreen);
     }
-
-    public void toggleHomeScreen()
+    public void toggleScreen(GameObject screen)
     {
-
-    }
-
-    public void toggleHighScoreListScreen()
-    {
-
+        if (screen.activeInHierarchy)
+        {
+            screen.SetActive(false);
+        }
+        else
+        {
+            screen.SetActive(true);
+        }
     }
 }
